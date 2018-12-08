b191fc36b111adf962c6f6fa0a0f53fb21a1e929
“the574th” <sufyazman@gmail.com>
2018-12-08 23:35:42 +0800

    Edit eventlistener/queryselector to select the last element instead of the first.

--- project-1/script.js
+++ project-1/script.js
@@ -1,37 +1,47 @@
-
-// var redGang = [];
-// var numRedBoxes = 3;
-
 var familyOfBoxes = {
     red: {
         array: [],
-        numBoxes: 3,
         classBorder: 'redBoxBorder',
+        classFill: 'redBox',
+        boxid: 'r',
         containerId: '#redBoxesMatters'
     },
     yellow: {
         array: [],
-        numBoxes: 4,
         classBorder: 'yellowBoxBorder',
+        classFill: 'yellowBox',
+        boxid: 'y',
         containerId: '#yellowBoxesMatters'
+    },
+    green: {
+        array: [],
+        classBorder: 'greenBoxBorder',
+        classFill: 'greenBox',
+        boxid: 'g',
+        containerId: '#greenBoxesMatters'
     }
 };
 
+
+//variables for colors, easier to reference
 var red = familyOfBoxes.red;
 var yellow = familyOfBoxes.yellow;
+var green = familyOfBoxes.green;
 
-var makeBoxes = function(color) {
-//Create the number of red boxes
-    for (var i=0; i<color.numBoxes; i++) {
+
+var makeBoxes = function(color, numOfBoxes) {
+    numOfBoxes = parseInt(numOfBoxes);
+//      Create the number of boxes by adding div
+    for (var i=0; i<numOfBoxes; i++) {
         color.array.push( document.createElement("div") );
     }
 
-//Gives each boxes an id and class
+//      Gives each boxes an id and class
         color.array.forEach(function(box, i){
-            box.id = i;
+            box.id = color.boxid+i;
             box.classList.add(color.classBorder);
 
-//Appends the boxes into the DOM
+//      Appends the boxes into the DOM
             var whereTheyAre = document.querySelector(color.containerId);
             whereTheyAre.appendChild(box);
         });
@@ -41,21 +51,29 @@ var makeBoxes = function(color) {
 var press = function (e) {
 //When you press 'a', this will happen..
      if (e.keyCode === 65) {
-        var first = document.querySelector(".redBoxBorder");
-        first.classList.replace('redBoxBorder', 'redBox');
-        console.log('can')
+        var all = document.querySelectorAll(".redBoxBorder");
+        var last = all[all.length-1];
+        last.classList.replace('redBoxBorder', 'redBox');
      }
 //When you press 's', this will happen..
      if (e.keyCode === 83) {
-        var first = document.querySelector(".yellowBoxBorder");
-        first.classList.replace('yellowBoxBorder', 'yellowBox');
-        console.log('can')
+        var all = document.querySelectorAll(".yellowBoxBorder");
+        var last = all[all.length-1];
+        last.classList.replace('yellowBoxBorder', 'yellowBox');
+     }
+//When you press 's', this will happen..
+     if (e.keyCode === 68) {
+        var all = document.querySelectorAll(".greenBoxBorder");
+        var last = all[all.length-1];
+        last.classList.replace('greenBoxBorder', 'greenBox');
      }
 }
 
+
 window.onload = function() {
-    makeBoxes(yellow);
-    makeBoxes(red);
+    makeBoxes(yellow, 5);
+    makeBoxes(red, 3);
+    makeBoxes(green, 4)
     document.addEventListener('keydown',press)
 }
 
