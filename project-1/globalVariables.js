7943b5e10e886458cc754db50fe84190a9f8a9db
“the574th” <sufyazman@gmail.com>
2018-12-12 11:05:37 +0800

    Fixed godfather GeneratePatternV2() new file for global variables

---
+++ project-1/globalVariables.js
@@ -0,0 +1,47 @@
+var familyOfBoxes = {
+    red: {
+        array: [],
+        classBorder: 'redBoxBorder',
+        classFill: 'redBox',
+        classAniHide: 'redBoxHide',
+        classWrong: 'redBoxWrong',
+        boxid: 'r',
+        containerId: '#redBoxesMatters',
+        numOfClicks: 0
+    },
+    yellow: {
+        array: [],
+        classBorder: 'yellowBoxBorder',
+        classFill: 'yellowBox',
+        classAniHide: 'yellowBoxHide',
+        classWrong: 'yellowBoxWrong',
+        boxid: 'y',
+        containerId: '#yellowBoxesMatters',
+        numOfClicks: 0
+    },
+    green: {
+        array: [],
+        classBorder: 'greenBoxBorder',
+        classFill: 'greenBox',
+        classAniHide: 'greenBoxHide',
+        classWrong: 'greenBoxWrong',
+        boxid: 'g',
+        containerId: '#greenBoxesMatters',
+        numOfClicks: 0
+    }
+};
+
+var gamePlay = {
+    score: 0,
+    name: null,
+    time: 30,
+    level: 1,
+    createNewPattern: 'yes',
+    colorsInvolvedInPattern: [],
+    totalDiv: 0,
+}
+
+//      variables for colors, for easier reference
+var red = familyOfBoxes.red;
+var yellow = familyOfBoxes.yellow;
+var green = familyOfBoxes.green;
